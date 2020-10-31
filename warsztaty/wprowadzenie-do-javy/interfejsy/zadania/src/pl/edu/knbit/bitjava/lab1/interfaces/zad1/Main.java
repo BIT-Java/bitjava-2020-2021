d27e8671138f2cea5b2220c226bf57fe05a5d90b
mzlnk <dev.zielonka.marcin@gmail.com>
2020-10-31 14:32:10 +0100

    added lab1-interfaces

---
+++ warsztaty/wprowadzenie-do-javy/interfejsy/zadania/src/pl/edu/knbit/bitjava/lab1/interfaces/zad1/Main.java
@@ -0,0 +1,29 @@
+package pl.edu.knbit.bitjava.lab1.interfaces.zad1;
+
+public class Main {
+
+    /*
+
+    Ćwiczenie 1:
+
+    Skopiuj klasy z pakietu pl.edu.knbit.bitjava.lab1.interfaces.figures z folder przyklady.
+
+    Następnie:
+    - stwórz nową klasę Circle, która będzie dziedziczyć po klasie Base2DFigure (pamiętaj o zaimplementowaniu metody z interfejsu Figure)
+    - dodaj do interfejsu nową metodę void rotate(int angle)
+    - zaimplementuj metodę rotate w klasach Base2DFigure i Base3DFigure (zauważ, że wtedy nie trzeba już implementować tej metody
+      we wszystkich podklasach (tj. Cube, Circle, Triangle, itd.). Ciałem metody może być zwykłe wypisanie tekstu na konsolę
+    - w klasie Triangle usuń metodę rotate()
+    - w klasie Circle nadpisz metodę rotate() tak aby wypisywała na konsole tekst "Nothing changed - I'm circle"
+
+    Następnie w metodzie main:
+    - stwórz tablicę różnych figur (w tym przynajmniej jedna typu Circle)
+    -  w pętli for-each wywołaj metody fill() i rotate()
+
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+}
