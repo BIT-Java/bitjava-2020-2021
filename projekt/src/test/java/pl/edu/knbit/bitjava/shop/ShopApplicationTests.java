592188864614648adf2611898f333c6d3b1a367c
Mateusz Surjak <surjak1999@gmail.com>
2020-11-23 18:26:40 +0100

    Spring start

---
+++ projekt/src/test/java/pl/edu/knbit/bitjava/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.knbit.bitjava.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
